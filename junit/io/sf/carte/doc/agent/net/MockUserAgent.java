86c46e091d9ce1c6f067a59e86204e8629e69a6c
Carlos Amengual <carlosame644+github@gmail.com>
2020-12-11 21:38:39 +0100

    Tests: add test for DefaultUserAgent.
    (cherry picked from commit 472b563d08ee28515baed58e96ac88a29da25620)

---
+++ junit/io/sf/carte/doc/agent/net/MockUserAgent.java
@@ -0,0 +1,45 @@
+/*
+
+ Copyright (c) 2005-2020, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.doc.agent.net;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.EnumSet;
+
+import io.sf.carte.doc.agent.DeviceFactory;
+import io.sf.carte.doc.agent.MockURLConnectionFactory;
+import io.sf.carte.doc.style.css.nsac.Parser.Flag;
+import io.sf.carte.doc.style.css.om.DummyDeviceFactory;
+
+class MockUserAgent extends DefaultUserAgent {
+
+	private static final long serialVersionUID = 1L;
+
+	private final MockURLConnectionFactory urlFactory = new MockURLConnectionFactory();
+
+	public MockUserAgent(EnumSet<Flag> parserFlags, boolean setDefaultSheet) {
+		super(parserFlags, setDefaultSheet);
+		DeviceFactory deviceFactory = new DummyDeviceFactory();
+		getDOMImplementation().setDeviceFactory(deviceFactory);
+	}
+
+	public MockURLConnectionFactory getConnectionFactory() {
+		return urlFactory;
+	}
+
+	@Override
+	protected URLConnection openConnection(URL url, long creationDate) throws IOException {
+		return urlFactory.createConnection(url);
+	}
+
+}
